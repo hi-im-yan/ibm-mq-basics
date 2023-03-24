37ab40f6b1a9fd761b7b7e2b46f0f3a7b7d968f4
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 16:17:58 -0400

    test: removed

---
+++ topic-queue-listener/src/test/java/com/ibmmqbasics/topicqueuelistener/TopicQueueListenerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.ibmmqbasics.topicqueuelistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicQueueListenerApplicationTests {
+
+}
