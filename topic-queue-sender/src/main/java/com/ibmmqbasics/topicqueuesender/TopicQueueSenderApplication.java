3c516ac87be7925005da3dfa15c9ae9c0ce9db70
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 15:20:36 -0400

    feat: enable jms

--- topic-queue-sender/src/main/java/com/ibmmqbasics/topicqueuesender/TopicQueueSenderApplication.java
+++ topic-queue-sender/src/main/java/com/ibmmqbasics/topicqueuesender/TopicQueueSenderApplication.java
@@ -2,8 +2,10 @@ package com.ibmmqbasics.topicqueuesender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class TopicQueueSenderApplication {
 
 	public static void main(String[] args) {
