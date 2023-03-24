2f15a9ab44a62bf1093353f03902c3ade3542496
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 16:26:00 -0400

    basic configurations for topic-listener

---
+++ topic-listener/src/main/java/com/ibmmqbasics/topiclistener/TopicListenerApplication.java
@@ -0,0 +1,13 @@
+package com.ibmmqbasics.topiclistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicListenerApplication.class, args);
+	}
+
+}
