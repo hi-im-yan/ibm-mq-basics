1bf5fe3268e48bc77a5abb983a028d329f23597b
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 16:34:48 -0400

    feat: enable jms

--- topic-listener/src/main/java/com/ibmmqbasics/topiclistener/TopicListenerApplication.java
+++ topic-listener/src/main/java/com/ibmmqbasics/topiclistener/TopicListenerApplication.java
@@ -2,8 +2,10 @@ package com.ibmmqbasics.topiclistener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class TopicListenerApplication {
 
 	public static void main(String[] args) {
