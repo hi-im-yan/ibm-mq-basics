0f142bc743711729dc9a14392b73977f63c48d29
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 16:42:12 -0400

    basic configurations for error-queue-listener

---
+++ error-queue-listener/src/main/java/com/ibmmqbasics/errorqueuelistener/ErrorQueueListenerApplication.java
@@ -0,0 +1,13 @@
+package com.ibmmqbasics.errorqueuelistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorQueueListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorQueueListenerApplication.class, args);
+	}
+
+}
---
+++ error-queue-listener/src/test/java/com/ibmmqbasics/errorqueuelistener/ErrorQueueListenerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.ibmmqbasics.errorqueuelistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorQueueListenerApplicationTests {
+
+}
