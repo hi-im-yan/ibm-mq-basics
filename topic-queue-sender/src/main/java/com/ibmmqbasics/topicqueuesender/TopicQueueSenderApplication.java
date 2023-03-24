6739a7d325bd04fe2832108b8d8c98e54e0d8ded
Yan Ajiki <yan.ajiki@mjv.com.br>
2023-03-24 13:58:47 -0400

    basic configurations for topic-queue-sender

---
+++ topic-queue-sender/src/main/java/com/ibmmqbasics/topicqueuesender/TopicQueueSenderApplication.java
@@ -0,0 +1,13 @@
+package com.ibmmqbasics.topicqueuesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicQueueSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicQueueSenderApplication.class, args);
+	}
+
+}
---
+++ topic-queue-sender/src/test/java/com/ibmmqbasics/topicqueuesender/TopicQueueSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmmqbasics.topicqueuesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicQueueSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
